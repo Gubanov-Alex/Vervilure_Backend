dedc1d47b45fbc07777afe1599e8f921cc8a6694
oleksandr <future_htm@gmail.com>
2025-06-19 13:18:35 +0300

    chore: clean up comments and redundant code in utils module initialization

--- src/apps/accounts/utils/__init__.py
+++ src/apps/accounts/utils/__init__.py
@@ -1,20 +1,5 @@
 """Utils initialization module."""
 
-
 from .oauth_validators import GoogleOAuthValidator
 
-# # Import only what's actually needed to avoid conflicts
-# try:
-#     from .oauth_validators import GoogleOAuthValidator
-#     __all__ = ["GoogleOAuthValidator"]
-# except ImportError:
-#     __all__ = []
-#
-# # Lazy import for EmailTester to avoid Django initialization issues
-# def get_email_tester():
-#     """Factory function for EmailTester with lazy import."""
-#     from .email_testing import EmailTester
-#     return EmailTester
-#
-# # Don't import EmailTester at module level to avoid Django setup issues
-# # Instead, provide factory function for accessing it
+__all__ = ["GoogleOAuthValidator"]
